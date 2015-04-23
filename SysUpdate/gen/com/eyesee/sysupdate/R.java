4d125fa8b1d19e0ce7db1768bf32e0d3042daff6
mark <mark@Win7-PC>
2015-04-23 15:03:08 +0800

    完成了系统升级的Apk界面开发

--- SysUpdate/gen/com/eyesee/sysupdate/R.java
+++ SysUpdate/gen/com/eyesee/sysupdate/R.java
@@ -10,6 +10,9 @@ package com.eyesee.sysupdate;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int white=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,25 +20,53 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int button_text_size=0x7f050007;
+        public static final int matrix_height=0x7f050006;
+        public static final int matrix_width=0x7f050005;
+        public static final int title_size=0x7f050002;
+        public static final int update_content_size=0x7f050003;
+        public static final int version_text_size=0x7f050004;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int back=0x7f020000;
+        public static final int back_d=0x7f020001;
+        public static final int bg=0x7f020002;
+        public static final int button=0x7f020003;
+        public static final int button_d=0x7f020004;
+        public static final int button_selector=0x7f020005;
+        public static final int ib_back_selector=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int pb_progress_drawable=0x7f020008;
+        public static final int progress_bar=0x7f02000d;
+        public static final int title=0x7f020009;
+        public static final int transparent_background=0x7f02000c;
+        public static final int update_content_bg=0x7f02000a;
+        public static final int update_version_bg=0x7f02000b;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090005;
+        public static final int ib_back=0x7f090000;
+        public static final int pb_download=0x7f090003;
+        public static final int tv_button=0x7f090004;
+        public static final int tv_update_content=0x7f090001;
+        public static final int tv_version=0x7f090002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int check_update=0x7f060006;
+        public static final int hello_world=0x7f060002;
+        public static final int sys_update=0x7f060003;
+        public static final int test_update_content=0x7f060004;
+        public static final int test_version=0x7f060005;
     }
     public static final class style {
         /** 
@@ -59,10 +90,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
