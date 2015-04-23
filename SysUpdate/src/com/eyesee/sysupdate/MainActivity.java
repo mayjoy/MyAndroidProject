cfe12f0c8f4716385619c62cbf4c12f8d4b5dfb1
mark <mark@Win7-PC>
2015-04-23 15:24:29 +0800

    代码实现了返回键返回的功能

--- SysUpdate/src/com/eyesee/sysupdate/MainActivity.java
+++ SysUpdate/src/com/eyesee/sysupdate/MainActivity.java
@@ -1,16 +1,40 @@
 package com.eyesee.sysupdate;
 
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
+import com.lidroid.xutils.view.annotation.event.OnClick;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	
+	@ViewInject(R.id.tv_button)
+	private TextView tv_button;
+	@ViewInject(R.id.ib_back)
+	private ImageButton ib_back;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ViewUtils.inject(this);
 		
-		
+	}
+	
+	@OnClick({R.id.ib_back,R.id.tv_button})
+	public void onClick(View v){
+		switch (v.getId()) {
+		case R.id.ib_back:
+			finish();
+			break;
+
+		case R.id.tv_button:
+			break;
+		}
 	}
 }
