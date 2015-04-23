4dec250ad24e042788471cc0080a836a6d77de7a
mark <mark@Win7-PC>
2015-04-23 17:14:13 +0800

    升级XML文件的解析功能完成

--- SysUpdate/src/com/eyesee/sysupdate/MainActivity.java
+++ SysUpdate/src/com/eyesee/sysupdate/MainActivity.java
@@ -1,14 +1,13 @@
 package com.eyesee.sysupdate;
 
 import java.io.StringReader;
+import java.util.Locale;
 
 import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
 
 import com.lidroid.xutils.HttpUtils;
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.exception.HttpException;
-import com.lidroid.xutils.http.RequestParams;
 import com.lidroid.xutils.http.ResponseInfo;
 import com.lidroid.xutils.http.callback.RequestCallBack;
 import com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
@@ -20,7 +19,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
 import android.util.Xml;
-import android.view.Menu;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -33,6 +31,10 @@ public class MainActivity extends Activity {
 	public static String URL = "http://192.168.0.108:8080/controller/controller.shtml";
 	private Context context = this;
 	
+	private String downLoadUrl;
+	private String md5;
+	private String language;
+	
 	HttpUtils http = new HttpUtils();
 	
 	@ViewInject(R.id.tv_button)
@@ -44,13 +46,18 @@ public class MainActivity extends Activity {
 	@ViewInject(R.id.tv_version)
 	private TextView tv_version;
 	
+	@ViewInject(R.id.tv_update_content)
+	private TextView tv_update_content;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		ViewUtils.inject(this);
+		//显示当前版本号
 		tv_version.setText(getResources().getString(R.string.test_version)+VERSION);
+		//获取语言环境
+		language = getResources().getConfiguration().locale.getLanguage();
 	}
 	
 	@OnClick({R.id.ib_back,R.id.tv_button})
@@ -88,7 +95,26 @@ public class MainActivity extends Activity {
 					try {
 						XmlPullParser parser = Xml.newPullParser();
 						parser.setInput(new StringReader(result));
-					} catch (XmlPullParserException e) {
+						int eventType = parser.getEventType();//得到第一个事件类型
+						while (eventType != XmlPullParser.END_DOCUMENT){
+							switch (eventType) {
+							case XmlPullParser.START_TAG:
+								if("url".equals(parser.getName())){
+									downLoadUrl = parser.nextText();
+								}else if("md5".equals(parser.getName())){
+									md5 = parser.nextText();
+								}else if("description".equals(parser.getName())){
+									if(language.equals(parser.getAttributeValue(1))){
+										tv_update_content.setText(parser.nextText());
+									}
+								}
+								break;
+
+							}
+							eventType=parser.next();//进入下一个事件处理
+						}
+						
+					} catch (Exception e) {
 						e.printStackTrace();
 						Log.d("mark", "更新文档解析失败");
 					}
