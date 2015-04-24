05165dec5cab6cd0817da41b17663b58279769eb
mark <mark@Win7-PC>
2015-04-24 09:17:26 +0800

    默认下载进度为0

--- SysUpdate/gen/com/eyesee/sysupdate/R.java
+++ SysUpdate/gen/com/eyesee/sysupdate/R.java
@@ -62,9 +62,13 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
+        public static final int cancle=0x7f06000a;
         public static final int check_update=0x7f060006;
-        public static final int have_last_version=0x7f060007;
+        public static final int download_update=0x7f060007;
+        public static final int fail_download=0x7f060009;
+        public static final int have_last_version=0x7f06000b;
         public static final int hello_world=0x7f060002;
+        public static final int install_update=0x7f060008;
         public static final int sys_update=0x7f060003;
         public static final int test_update_content=0x7f060004;
         public static final int test_version=0x7f060005;
