d2096caed5283937ab5139e4dcfa643bb70445af
whqsj2013 <whqsj@126.com>
2015-05-25 14:47:20 +0800

    更新包名

--- AirLauncher2/src/com/eyesee/airlauncher2/utils/Constants.java
+++ AirLauncher2/src/com/eyesee/airlauncher2/utils/Constants.java
@@ -13,12 +13,11 @@ public interface Constants {
 	public static final String UPDATE_WEATHER = "updateWeather";
 	
 	public static final String AIRLAUNCHER_PACKAGE_NAME = "com.eyesee.airlauncher2";
-	public static final String MUSIC_PACKAGE_NAME = "com.kugou.android";
-	public static final String SYSUPDATE_PACKAGE_NAME = "com.eyesee.sysupdate";
+	public static final String MUSIC_PACKAGE_NAME = "com.kugou.android";//酷狗音乐
 	public static final String BTCALL_PACKAGE_NAME = "com.example.carairbluetooth2";
 	public static final String AIRSETTING_PACKAGE_NAME = "com.eyesee.CarAirSetting2";
-	public static final String FILEMANAGER_PACKAGE_NAME = "com.estrongs.android.pop";
-	public static final String NAV_PACKAGE_NAME = "com.baidu.BaiduMap";
+	public static final String FILEMANAGER_PACKAGE_NAME = "com.estrongs.android.pop";//es文件浏览器
+	public static final String NAV_PACKAGE_NAME = "com.baidu.BaiduMap";//百度地图
 	public static final String RECODER_PACKAGE_NAME = "com.aw.recorder";
 	
 }
