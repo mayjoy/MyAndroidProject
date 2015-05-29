f76da78ad7853a0e4b77612e51d773aa46784fbd
whqsj2013 <whqsj@126.com>
2015-05-29 10:16:27 +0800

    添加了常量注释

--- AirLauncher2/src/com/eyesee/airlauncher2/utils/Constants.java
+++ AirLauncher2/src/com/eyesee/airlauncher2/utils/Constants.java
@@ -14,10 +14,10 @@ public interface Constants {
 	
 	public static final String AIRLAUNCHER_PACKAGE_NAME = "com.eyesee.airlauncher2";
 	public static final String MUSIC_PACKAGE_NAME = "com.kugou.android";//酷狗音乐
-	public static final String BTCALL_PACKAGE_NAME = "com.example.carairbluetooth2";
-	public static final String AIRSETTING_PACKAGE_NAME = "com.eyesee.CarAirSetting2";
-	public static final String FILEMANAGER_PACKAGE_NAME = "com.estrongs.android.pop";//es文件浏览器
+	public static final String BTCALL_PACKAGE_NAME = "com.example.carairbluetooth2";//蓝牙拨号
+	public static final String AIRSETTING_PACKAGE_NAME = "com.eyesee.CarAirSetting2";//设置 *
+	public static final String FILEMANAGER_PACKAGE_NAME = "com.estrongs.android.pop";//es文件浏览器 *
 	public static final String NAV_PACKAGE_NAME = "com.baidu.BaiduMap";//百度地图
-	public static final String RECODER_PACKAGE_NAME = "com.aw.recorder";
+	public static final String RECODER_PACKAGE_NAME = "com.aw.recorder";//记录仪
 	
 }
